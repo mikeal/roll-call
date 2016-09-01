2c92352655ddcfe70710a8996ff9af8dc5b01611
Mikeal Rogers <mikeal.rogers@gmail.com>
2016-09-01 16:23:52 -0700

    incremental commit.

--- README.md
+++ README.md
@@ -19,3 +19,22 @@ If you want to debug the web interface run.
 ```bash
 npm run web
 ```
+
+## URL structure
+
+Because this is a single-page static application all actions are embedded in
+querystrings.
+
+### ?call=:id
+
+Where `id` is the identifier of the `current` user.
+
+If a user opens this URL it will ask for their information and the *call* the
+user embedded in the URL. Once the call is established the url will pushState
+to the new calling user's call URL.
+
+In effect, the *call* url is like a phone number for the current user. Anyone
+in an active call can take *their* url and give it to another user, that user
+will then call that user and they can add them to the call which will begin
+sharing the signalling information of the other users on the call.
+
--- renderer.js
+++ renderer.js
@@ -11,51 +11,83 @@
 //   }
 // })
 
-const ipfs = new Ipfs()
+// const ipfs = new Ipfs()
 const once = require('once')
 const bs58 = require('bs58')
 const concat = require('concat-stream')
+const crypto = require('crypto')
+const request = require('request')
+
+// TODO: temporarily replace this with a service.
 
 function write (obj, cb) {
   cb = once(cb)
-  ipfs.files.createAddStream((err, stream) => {
+  var id = crypto.randomBytes().toString('hex')
+  obj._id = id
+  console.log(write)
+  request.put('http://localhost:6688/v1/'+id, {json: obj}, (err, resp) => {
     if (err) return cb(err)
-    stream.once('error', cb)
-    stream.write({
-      path: 'test',
-      content: new Buffer(JSON.stringify(obj))
-    })
-    stream.on('data', file => {
-      console.log(file)
-      cb(null, bs58.encode(file.node.multihash()).toString())
-    })
-    stream.end()
+    var status = resp.statusCode
+    if (status !== 201) return cb(new Error('Status Code not 201, '+status))
+    cb(null, id)
   })
 }
 
-function get (hash, cb) {
+function link (name, obj, cb) {
   cb = once(cb)
-  ipfs.files.get(hash, (err, stream) => {
-    console.log('error getting hash', err)
+  var id = crypto.randomBytes().toString('hex')
+  obj._id = id
+  request.put('http://localhost:6688/v1/name/'+name, {json: obj}, (err, resp) => {
     if (err) return cb(err)
-    let ret = concat((data) => {
-      console.log(data.toString())
-      cb(null, JSON.parse(data.toString()))
-    })
-    stream.once('data', (obj) => {
-      obj.content.pipe(ret)
-    })
-    stream.on('error', (err) => console.log('stream error', err))
-    stream.on('error', cb)
-    ret.on('error', cb)
+    var status = resp.statusCode
+    if (status !== 201) return cb(new Error('Status Code not 201, '+status))
+    cb(null, id)
   })
+  return // Disable actual IPFS for now
+  // ipfs.files.createAddStream((err, stream) => {
+  //   if (err) return cb(err)
+  //   stream.once('error', cb)
+  //   stream.write({
+  //     path: 'test',
+  //     content: new Buffer(JSON.stringify(obj))
+  //   })
+  //   stream.on('data', file => {
+  //     console.log(file)
+  //     cb(null, bs58.encode(file.node.multihash()).toString())
+  //   })
+  //   stream.end()
+  // })
+}
+
+function get (name, cb) {
+  var url = 'http://localhost:6688/v1/name/'+name
+  request.get(url, {json: true}, (err, resp, obj) => {
+    if (err) return cb(err)
+    var status = resp.statusCode
+    if (status !== 200) return cb(new Error('Status Code not 200, '+status))
+    cb(null, obj)
+  })
+  return
+  // cb = once(cb)
+  // ipfs.files.cat(hash, (err, stream) => {
+  //   console.log('error getting hash', err)
+  //   if (err) return cb(err)
+  //   let ret = concat((data) => {
+  //     console.log(data.toString())
+  //     cb(null, JSON.parse(data.toString()))
+  //   })
+  //   stream.pipe(ret)
+  //   stream.on('error', (err) => console.log('stream error', err))
+  //   stream.on('error', cb)
+  //   ret.on('error', cb)
+  // })
 }
 
 const qs = require('querystring')
 var opts = qs.parse(window.location.search.slice(1))
 
 if (!opts.offer) {
-  createOffer()
+  createUser()
 } else {
   console.log("reading...")
   get(opts.offer, (err, obj) => {
@@ -63,16 +95,21 @@ if (!opts.offer) {
   })
 }
 
-function createOffer (cb) {
+function createUser (obj, cb) {
+  console.log('creating offer')
   var SimplePeer = require('simple-peer')
 
   var me = new SimplePeer({ initiator: true, trickle: false })
 
+  console.log('created peer')
+
   me.once('signal', function (data) {
     // when peer1 has signaling data, give it to peer2 somehow
+    console.log('offer', data)
     write(data, (err, hash) => {
       console.log('hash', hash)
-      get(hash, (err, obj) => {
+      console.log(hash)
+      get('test', (err, obj) => {
         console.log(err, obj)
         window.history.replaceState({}, null, '?offer='+hash)
       })
@@ -80,7 +117,3 @@ function createOffer (cb) {
     console.log('signal', data)
   })
 }
-
-window.simpleIpfs = {get: get, put: write}
-
-get('QmZTR5bcpQD7cFgTorqxZDYaew1Wqgfbd2ud9QqGPAkK2V', () => {})
