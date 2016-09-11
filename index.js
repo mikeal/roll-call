7c2b811521b3d106f677f393ca70ff9ccc8b4314
Mikeal Rogers <mikeal.rogers@gmail.com>
2016-09-11 00:19:33 -0700

    Adding most of the UI components.

--- index.js
+++ index.js
@@ -2,9 +2,75 @@ const createSwarm = require('killa-beez')
 const funky = require('funky')
 const getUserMedia = require('getusermedia')
 const qs = require('querystring')
+const recordRTC = require('recordrtc')
+const bel = require('bel')
+const values = (obj) => Object.keys(obj).map(k => obj[k])
 
 let signalHost = 'https://signalexchange.now.sh'
 let roomHost = 'https://roomexchange.now.sh'
+
+let myMicrophone
+let mySwarm
+let myRecorder
+
+const recordButton = bel`
+<button id="record" class="ui compact labeled icon button">
+  <i class="unmute icon"></i>
+    Record
+</button>
+`
+
+function stopRecording (cb) {
+  // TODO: stop recording
+  // update the URL
+  $(recordButton).remove()
+  values(mySwarm.remotes).forEach(remote => {
+    remote.stopRecording(err => {
+      // TODO: update UI
+    })
+  })
+  myRecorder.stopRecording(audioURL => {
+    let call = document.getElementById('aundefined')
+    let audio = new Audio()
+    let name = call.querySelector('div.header').textContent
+    let download = downloadView()
+    let upload = uploadView()
+    let localTrack = connectAudio(audio, false, localTrackView)
+
+    let track = trackView({call, name, audio, download, upload, localTrack})
+    document.getElementById('tracks-container').appendChild(track)
+    audio.src = audioURL
+
+    $(call.querySelector('div.header')).remove()
+
+    var recordedBlob = myRecorder.getBlob()
+    // record.getDataURL(function(dataURL) { })
+    if (cb && typeof cb === 'function') cb(null)
+  })
+}
+
+function startRecording (local) {
+  console.log('startRecording')
+  let opts = {disableLogs: true, type: 'audio'}
+  let record = recordRTC(myMicrophone, opts)
+
+  if (local) {
+    values(mySwarm.remotes).forEach(remote => {
+      console.log('calling remote record')
+      remote.record(err => {
+        if (err) return console.error(err)
+        console.log('calling remote record finished')
+        // TODO: add the recording animation to this person
+      })
+    })
+  }
+  record.startRecording()
+  recordButton.innerHTML = `<i class="stop icon"></i> Stop`
+  recordButton.onclick = () => stopRecording()
+  myRecorder = record
+}
+
+recordButton.onclick = startRecording
 
 function joinRoom (room) {
   room = `peer-call:${room}`
@@ -12,7 +78,8 @@ function joinRoom (room) {
   getUserMedia(mediaopts, (err, audioStream) => {
     if (err) return console.error(err)
     if (!audioStream) return console.error("no audio")
-    window.audioStream = audioStream
+    myMicrophone = audioStream
+    window.myMicrophone = myMicrophone
     let p = addPerson(audioStream)
     let swarm = createSwarm(signalHost, {stream: audioStream})
     swarm.joinRoom(roomHost, room)
@@ -22,12 +89,27 @@ function joinRoom (room) {
       audio.src = URL.createObjectURL(stream)
       stream.publicKey = stream.peer.publicKey
       let elem = addPerson(stream, true)
+      elem.audioStream = stream
+      let remotes = values(swarm.peers).length
+      elem.querySelector('div.person-name').textContent = `Caller (${remotes})`
       document.getElementById('audio-container').appendChild(elem)
     })
     swarm.on('disconnect', pubKey => {
       $(document.getElementById(`a${pubKey}`)).remove()
     })
+    mySwarm = swarm
     document.getElementById('audio-container').appendChild(p)
+    document.body.appendChild(recordButton)
+
+    // Setup RPC Services
+    swarm.rpc.record = cb => {
+      console.log('rpc record')
+      startRecording(false)
+      cb(null)
+    }
+    swarm.rpc.stopRecording = cb => {
+      stopRecording()
+    }
   })
 }
 const mainButtons = funky`
@@ -40,7 +122,67 @@ const mainButtons = funky`
 </div>`
 
 const remoteAudio = funky`
-  <div class="card" id="a${id => id}">
+<div class="card" id="a${id => id}">
+  <div style="height:49px;width:290">
+    <canvas id="canvas"
+      width="290"
+      height="49"
+      class="person"
+      >
+    </canvas>
+  </div>
+  <div class="extra content">
+    <div contenteditable="true" class="header person-name">Me</div>
+    <div class="volume">
+      <div class="ui toggle checkbox">
+        <input type="checkbox" name="mute">
+        <label>Mute</label>
+      </div>
+      <input type="range" min="0" max="2" step=".05" />
+    </div>
+  </div>
+</div>
+`
+
+const downloadView = funky`
+<div class="card" id="b${id => id}">
+  <div class="content">
+    <div class="ui active progress">
+      <div class="bar">
+        <div class="progress"></div>
+      </div>
+      <div class="label">Download Their Recording</div>
+    </div>
+  </div>
+</div>
+`
+const uploadView = funky`
+<div class="card" id="c${id => id}">
+  <div class="content">
+    <div class="ui active progress">
+      <div class="bar">
+        <div class="progress"></div>
+      </div>
+      <div class="label">Uploading My Audio</div>
+    </div>
+  </div>
+</div>
+`
+
+const trackView = funky`
+<div class="ui segment track-container">
+  <div class="ui top attached label">${info => info.name}</div>
+  <div class="ui special cards">
+    ${info => info.download || ''}
+    ${info => info.upload || ''}
+    ${info => info.localTrack}
+    ${info => info.call}
+  </div>
+</div>
+`
+
+const localTrackView = funky`
+  <div class="card" id="d${id => id}">
     <div style="height:49px;width:290">
       <canvas id="canvas"
         width="290"
@@ -51,10 +193,7 @@ const remoteAudio = funky`
     </div>
     <div class="extra content">
       <div class="volume">
-        <div class="ui toggle checkbox">
-          <input type="checkbox" name="mute">
-          <label>Mute</label>
-        </div>
+        <label>Gain</label>
         <input type="range" min="0" max="2" step=".05" />
       </div>
     </div>
@@ -91,6 +230,7 @@ function startLoop () {
       var barWidth = (WIDTH / bufferLength) * 5
       var barHeight
       var x = 0
+      var total = 0
       for (var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i] / 3
         if (barHeight > 10) {
@@ -98,8 +238,15 @@ function startLoop () {
           canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight)
         }
         x += barWidth + 1
+        total += barHeight
       }
       lastTime = now
+
+      if (total > 1000) {
+        $(canvas.parentNode.parentNode).addClass('pulse')
+      } else {
+        $(canvas.parentNode.parentNode).removeClass('pulse')
+      }
     }
   }
   draw()
@@ -108,11 +255,17 @@ function startLoop () {
 
 let context = new AudioContext()
 
-function addPerson (stream, play) {
-  let element = remoteAudio(stream.publicKey)
+function connectAudio (stream, play, view) {
+  let element = view(stream.publicKey)
   let volume = context.createGain()
   let analyser = context.createAnalyser()
-  let source = context.createMediaStreamSource(stream)
+  let source
+  if (stream instanceof MediaStream) {
+    source = context.createMediaStreamSource(stream)
+  } else {
+    source = context.createMediaElementSource(stream)
+  }
+
   let volumeSelector = 'input[type=range]'
   let muteSelector = 'input[type=checkbox]'
   let muteElement = element.querySelector(muteSelector)
@@ -153,6 +306,10 @@ function addPerson (stream, play) {
   element.volume = volume
 
   return element
+}
+
+function addPerson (stream, play) {
+  return connectAudio(stream, play, remoteAudio)
 }
 
 function ask () {
