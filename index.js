eaa2c89ded25850233d64ad3000c989c49955131
Feross Aboukhadijeh <feross@feross.org>
2016-10-10 20:50:17 -0700

    Timeout after waiting for RTC config for 10 seconds
    https://instant.io has excellent uptime, but if it's down for some
    reason, then the XHR request should fail instantly. This timeout is
    just for if the server is hung or really slow for some reason.

--- index.js
+++ index.js
@@ -269,7 +269,10 @@ function recording (swarm, microphone) {
 }
 
 function getRtcConfig (cb) {
-  xhr('https://instant.io/rtcConfig', function (err, res) {
+  xhr({
+    url: 'https://instant.io/rtcConfig',
+    timeout: 10000
+  }, function (err, res) {
     if (err || res.statusCode !== 200) {
       cb(new Error('Could not get WebRTC config from server. Using default (without TURN).'))
     } else {
