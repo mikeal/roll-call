6ef064e57a4def73d0fc1abe2d4c233006f441b0
Max Goodman <c@chromakode.com>
2016-10-16 14:24:04 -0700

    Use circle icon for record button (#42)

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const waudio = require('waudio')(context)
 
 const recordButton = bel `
 <button id="record" class="ui compact labeled icon button">
-  <i class="unmute icon"></i>
+  <i class="circle icon"></i>
   <span>Record</span>
 </button>
 `
